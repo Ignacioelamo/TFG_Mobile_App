bf2b0b1fa5f3b3b0434e481c6446369e6a143d35
Antonio :) <ad.lopeztardido@gmail.com>
2025-04-01 01:58:01 +0200

    Imports sin utilizar + Correccion de la jerarquía de creacion de widgets + Comprobacion de existencia de widget antes de creacion + Uso del spread + Refactorizaciones menores de codigo

--- lib/view/my_app.dart
+++ lib/view/my_app.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // Importa DateFormat
 import 'package:path_provider/path_provider.dart';
 
 class MyApp extends StatefulWidget {
@@ -14,28 +13,22 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TFG MOBILE APP',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TFG MOBILE APP'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.info_outlined),
-              onPressed: () async {
-                _showConfirmationDialog(context);
-              },
-            ),
-          ],
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: _openFileDirectory,
-            child: const Text('Ver archivos guardados'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TFG MOBILE APP'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.info_outlined),
+            onPressed: () async {
+              _showConfirmationDialog(context);
+            },
           ),
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _openFileDirectory,
+          child: const Text('Ver archivos guardados'),
         ),
       ),
     );
@@ -55,9 +48,16 @@ class MyAppState extends State<MyApp> {
                   text: const TextSpan(
                     style: TextStyle(fontSize: 16.0, color: Colors.black),
                     children: <TextSpan>[
-                      TextSpan(text: 'Esta aplicación ha sido realizada bajo el proyecto '),
-                      TextSpan(text: 'Continuous decentralized Learning of IoT Device\'s Behavioural Profiles', style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(text: ' y se han seguido todo el reglamento de la comisión de ética de la Universidad de Murcia a la hora de recoger los datos.'),
+                      TextSpan(
+                          text:
+                              'Esta aplicación ha sido realizada bajo el proyecto '),
+                      TextSpan(
+                          text:
+                              'Continuous decentralized Learning of IoT Device\'s Behavioural Profiles',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text:
+                              ' y se han seguido todo el reglamento de la comisión de ética de la Universidad de Murcia a la hora de recoger los datos.'),
                     ],
                   ),
                 ),
@@ -79,8 +79,9 @@ class MyAppState extends State<MyApp> {
 
   Future<void> _openFileDirectory() async {
     final directory = await getExternalStorageDirectory();
-    final fileList = directory?.listSync().map((item) => item as File).toList() ?? [];
-    // Encuentra el archivo más recientemente modificado
+    final fileList =
+        directory?.listSync().map((item) => item as File).toList() ?? [];
+
     File? latestFile;
     DateTime? lastModified;
     for (File file in fileList) {
@@ -91,6 +92,8 @@ class MyAppState extends State<MyApp> {
       }
     }
 
+    if (!mounted) return;
+
     showDialog<void>(
       context: context,
       builder: (BuildContext context) {
@@ -99,15 +102,18 @@ class MyAppState extends State<MyApp> {
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
-                Text(directory?.path ?? "Ruta no disponible", style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(directory?.path ?? "Ruta no disponible",
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
                 ...fileList.map((file) => ListTile(
-                  title: Text(file.path.split('/').last),
-                  onTap: () => Navigator.of(context).pop(),
-                )).toList(),
-                if (latestFile != null && lastModified != null) Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Text("Último modificado: ${latestFile.path.split('/').last} hace ${DateTime.now().difference(lastModified).inMinutes} minutos"),
-                ),
+                      title: Text(file.path.split('/').last),
+                      onTap: () => Navigator.of(context).pop(),
+                    )),
+                if (latestFile != null && lastModified != null)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Text(
+                        "Último modificado: ${latestFile.path.split('/').last} hace ${DateTime.now().difference(lastModified).inMinutes} minutos"),
+                  ),
               ],
             ),
           ),
@@ -124,7 +130,3 @@ class MyAppState extends State<MyApp> {
     );
   }
 }
-
-
-
-
